2509bbce25e639245ae1ff955001bc61e88c7b6a
halimegldn <87153723+halimegldn@users.noreply.github.com>
2023-09-20 20:09:23 +0300

    Add files via upload

---
+++ kullan__c__lar.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 20 Eyl 2023, 19:08:36
+-- Sunucu sürümü: 10.4.28-MariaDB
+-- PHP Sürümü: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `kullanici_bilgi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `kullanıcılar`
+--
+
+CREATE TABLE `kullanıcılar` (
+  `kullanıcı_adı` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefon` int(100) NOT NULL,
+  `sifre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `kullanıcılar`
+--
+ALTER TABLE `kullanıcılar`
+  ADD PRIMARY KEY (`kullanıcı_adı`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
